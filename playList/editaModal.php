6bbbda91243555f47fbafc27b728a78fe25ad2a1
Camargo Lizbeth <201904983@est.umss.edu>
2023-10-25 06:59:52 -0400

    mensajes para los modal

--- playList/editaModal.php
+++ playList/editaModal.php
@@ -1,26 +1,28 @@
-<!-- Modal Editar modal  -->
+<!-- Modal Editar modal -->
 <div class="modal fade" id="editaModal" tabindex="-1" aria-labelledby="editaModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-            <h1 class="modal-title fs-5" id="nuevoModalLabel" style="color: white;">Editar Información</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <h1 class="modal-title fs-5" id="nuevoModalLabel" style="color: white;">Editar Información</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-            <form action="actualiza.php" method="post" enctype="multipart/form-data">
+                <form action="actualiza.php" method="post" enctype="multipart/form-data">
 
-                <input type="hidden" id="id" name="id">
+                    <input type="hidden" id="id" name="id">
 
                     <div class="mb-2">
                         <label for="nombre" class="form-label">Nombre:</label>
-                        <input type="text" name="nombre" id="nombre" class="form-control" required>
+                        <input type="text" name="nombre" id="nombre" maxlength="100" class="form-control" required>
+                        <span id="nombre-error-edit" style="color: #9cd2d3;"></span>
+
                     </div>
 
                     <div class="mb-2">
                         <label for="descripcion" class="form-label">Descripción:</label>
-                        <textarea name="descripcion" id="descripcion" class="form-control" rows="3" required></textarea>
+                        <textarea name="descripcion" id="descripcion" class="form-control" rows="3" maxlength="120"></textarea>
                     </div>
-                    
+
                     <div class="mb-2">
                         <img id="img_imagen" width="380" class="img-thumbnail">
                     </div>
@@ -29,7 +31,7 @@
                         <input type="file" name="imagen" id="imagen" class="form-control" accept="image/jpeg">
                     </div>
 
-                    <div class="d-flex justify-content-end pt2">
+                    <div class="d-flex justify-content-end pt-2">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
                         <button type="submit" class="btn btn-primary"><i class="fa-solid fa-floppy-disk"></i> Guardar</button>
                     </div>
@@ -40,3 +42,17 @@
         </div>
     </div>
 </div>
+
+<script>
+    // Validación para el campo "nombre" en el modal de edición
+    document.getElementById('nombre').addEventListener('input', function () {
+        var nombre = this.value;
+        if (nombre.length > 100) {
+            document.getElementById('nombre-error-edit').textContent = 'El nombre no puede tener más de 100 caracteres.';
+            this.setCustomValidity('El nombre no puede tener más de 100 caracteres.');
+        } else {
+            document.getElementById('nombre-error-edit').textContent = '';
+            this.setCustomValidity('');
+        }
+    });
+</script>
--- playList/playList.php
+++ playList/playList.php
@@ -19,35 +19,28 @@ $dir = "imagen/";
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/all.min.css" rel="stylesheet">
     <style>
-        .btn-primary {
-    bs-btn-color: #fff;
-    bs-btn-bg: while; 
-    bs-btn-border-color: while;
-        }
         /* Estilo personalizado para las tarjetas */
         .card {
-            height: 345px;
-            background-color: #f2e6cf; /* Cambia el color de fondo a azul */
-
+            height: 320px;
+            background-color: #214252; /* Cambia el color de fondo a azul */
         }
         .modal-content {
-        background-color: #214252; /* Cambia el color de fondo del modal a azul */
+            background-color: #214252; /* Cambia el color de fondo del modal a azul */
         }
         .modal-content .modal-body {
-        color: white; /* Cambia el color del texto a blanco */
+            color: white; /* Cambia el color del texto a blanco */
         }
         .card-img-top {
             height: 150px;
             object-fit: cover;
         }
         /* Estilo personalizado para los botones */
-     .btn-editar {
+        .btn-editar {
             background-color: #0799b6;
             color: white;
         }
-
         .btn-eliminar {
-            background-color:#056496;
+            background-color: #056496;
             color: white;
         }
     </style>
@@ -56,7 +49,7 @@ $dir = "imagen/";
 <body class="d-flex flex-column h-100">
 
     <div class="container py-3">
-        <h2 class="text-left" style="color: white;" >Mi Biblioteca</h2>
+        <h2 class="text-left" style="color: white;">Mi Biblioteca</h2>
         <hr>
 
         <?php if (isset($_SESSION['msg']) && isset($_SESSION['color'])) { ?>
@@ -75,24 +68,31 @@ $dir = "imagen/";
             </div>
         </div>
 
-        <div class="row">
-            <?php while ($row = $playlists->fetch_assoc()) { ?>
-                <div class="col-md-4 mb-4">
-                    <div class="card">
-                       
-                    <td><img src="<?= $dir . $row['id'] . '.jpg?n=' . time(); ?>" width="345" style="height: 220px; width: 100%;"></td>
-
-                        <div class="card-body">
-                            <h5 class="card-title"><?= $row['nombre']; ?></h5>
-                        </div>
-                        <div class="card-footer">
-                            <a href="#" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editaModal" data-bs-id="<?= $row['id']; ?>"><i class="fa-solid fa-pen-to-square"></i> Editar</a>
-                            <a href="#" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#eliminaModal" data-bs-id="<?= $row['id']; ?>"><i class="fa-solid fa-trash"></i> Eliminar</a>
+          <div class="row">
+    <?php while ($row = $playlists->fetch_assoc()) { ?>
+        <div class="col-md-4 mb-4">
+        <div class="card" style="background-color: #214252; color: white;">
+                <td><img src="<?= $dir . $row['id'] . '.jpg?n=' . time(); ?>" width="300" style="height: 200px; width: 100%;"></td>
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h5 class="card-title" style="overflow: hidden; text-overflow: ellipsis; white-space: nowrap; max-width: 80%;"><?= $row['nombre']; ?></h5>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                ...
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#editaModal" data-bs-id="<?= $row['id']; ?>"><i class="fa-solid fa-pen-to-square"></i> Editar</a>
+                                <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#eliminaModal" data-bs-id="<?= $row['id']; ?>"><i class="fa-solid fa-trash"></i> Eliminar</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            <?php } ?>
+            </div>
         </div>
+    <?php } ?>
+</div>
+
+
     </div>
 
     <?php include 'nuevoModal.php'; ?>
@@ -114,14 +114,12 @@ $dir = "imagen/";
             nuevoModal.querySelector('.modal-body #descripcion').value = ""
             nuevoModal.querySelector('.modal-body #imagen').value = ""
 
-            
         })
 
         editaModal.addEventListener('hide.bs.modal', event => {
             editaModal.querySelector('.modal-body #nombre').value = ""
             editaModal.querySelector('.modal-body #descripcion').value = ""
             editaModal.querySelector('.modal-body #img_imagen').value = ""
-
         })
 
         editaModal.addEventListener('shown.bs.modal', event => {
@@ -133,46 +131,39 @@ $dir = "imagen/";
             let inputDescripcion = editaModal.querySelector('.modal-body #descripcion')
             let imagen = editaModal.querySelector('.modal-body #img_imagen')
 
-
-            let url="getPlaylist.php"
+            let url = "getPlaylist.php"
             let formData = new FormData()
             formData.append('id', id)
 
             fetch(url, {
-                    method: "POST",
-                    body: formData
-                }).then(response => response.json())
+                method: "POST",
+                body: formData
+            }).then(response => response.json())
                 .then(data => {
-
                     inputId.value = data.id
                     inputNombre.value = data.nombre
                     inputDescripcion.value = data.descripcion
                     imagen.src = '<?= $dir ?>' + data.id + '.jpg'
-
-
                 }).catch(err => console.log(err))
-
         })
 
         eliminaModal.addEventListener('shown.bs.modal', event => {
             let button = event.relatedTarget
             let id = button.getAttribute('data-bs-id')
             eliminaModal.querySelector('.modal-footer #id').value = id
-
         })
-       
     </script>
-  
+
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <style>
-    body {
-        background-color: #214252;
-    }
-    table {
-        background-color: #f1f1f1;
-    }
-     
-</style>
+        body {
+            background-color: #214252;
+        }
+
+        table {
+            background-color: #f1f1f1;
+        }
+    </style>
 
 </body>
 

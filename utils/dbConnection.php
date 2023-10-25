ad7f35816de07d3b47e4ce1048fb8243a75b608b
Quispe Choque Michelle <quispemichellemq@gmail.com>
2023-10-25 11:05:46 -0400

    mostrar contraseña- checkbox

--- utils/dbConnection.php
+++ utils/dbConnection.php
@@ -1,6 +1,6 @@
 <?php
 // Database connection 
-$conn = mysqli_connect('localhost', 'root', '', 'spottplay321');
+$conn = mysqli_connect('localhost', 'root', '', 'spottplay');
 
 mysqli_set_charset($conn, "utf8");
 

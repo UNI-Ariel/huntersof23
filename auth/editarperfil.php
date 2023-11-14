ddfe63f45acddf93051bc8313b64c3d238cfac58
FuentesRiosBeatriz <146163950+FuentesRiosBeatriz@users.noreply.github.com>
2023-11-14 09:57:33 -0400

    Update editarperfil.php

--- auth/editarperfil.php
+++ auth/editarperfil.php
@@ -1,1 +1,21 @@
+<!-- ./auth/editarperfil.php -->
+
+
+<form action="./auth/handle-edit-profile.php" method="post">
+    <label for="newName">Nuevo Nombre:</label>
+    <input type="text" id="newName" name="newName" required>
+
+    
+
+    <input type="submit" value="Guardar Cambios">
+</form>
+
+<!-- ./auth/editarperfil.php -->
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Asegúrate de incluir las etiquetas meta y enlaces a CSS necesarios -->
+    <title>Editar Perfil - SpottPlay</title>
+</head>
 

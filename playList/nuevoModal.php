e1472ef3079fb08f127b26f06446f956ac7858c2
Camargo Lizbeth <201904983@est.umss.edu>
2023-11-15 01:10:51 -0400

    bug 207 msj en descripcion cuando excede caracteres

--- playList/nuevoModal.php
+++ playList/nuevoModal.php
@@ -13,20 +13,18 @@
                         <label for="nombre" class="form-label">Nombre:</label>
                         <input type="text" name="nombre" id="nombre" class="form-control" required>
                         <span id="nombre-error" style="color: #9cd2d3 ;"></span>
-
-
                     </div>
 
                     <div class="mb-3">
                         <label for="descripcion" class="form-label">Descripción:</label>
-                        <textarea name="descripcion" id="descripcion" class="form-control" rows="3" maxlength="120"></textarea>
+                        <textarea name="descripcion" id="descripcion" class="form-control" rows="3" ></textarea>
+                        <span id="descripcion-error" style="color: #9cd2d3;"></span>
                     </div>
 
                     <div class="mb-3">
                         <label for="imagen" class="form-label">Imagen:</label>
                         <input type="file" name="imagen" id="imagen" class="form-control" accept="image/jpeg, image/png, image/jpg">
                         <img id="imagenPreview" src="-playList-.png" width="353" height="199" />
-
                     </div>
 
                     <div class="">
@@ -44,13 +42,26 @@
     // Agregar una función para validar la longitud del campo "nombre" al cambiar
     document.getElementById('nombre').addEventListener('input', function () {
         var nombre = this.value;
-        if (nombre.length > 100) {
-            document.getElementById('nombre-error').textContent = 'El nombre no puede tener más de 100 caracteres.';
-            this.setCustomValidity('El nombre no puede tener más de 100 caracteres.');
+        if (nombre.length > 30) {
+            document.getElementById('nombre-error').textContent = 'Nombre no válido, el mínimo de caracteres permitidos es 1, y el máximo 30 . Por favor, escriba un nombre dentro de los parámetros';
+            this.setCustomValidity('Nombre no válido, el mínimo de caracteres permitidos es uno, y el máximo 30 . Por favor, escriba un nombre dentro de los parámetros');
         } else {
             document.getElementById('nombre-error').textContent = '';
             this.setCustomValidity('');
         }
     });
+
+    // Agregar una función para validar la longitud del campo "descripcion" al cambiar
+    document.getElementById('descripcion').addEventListener('input', function () {
+        var descripcion = this.value;
+        if (descripcion.length > 30) {
+            document.getElementById('descripcion-error').textContent = 'Número de caracteres excedido. (Máximo de 30 )';
+            this.setCustomValidity('Número de caracteres excedido. (Máximo de 30 )');
+        } else {
+            document.getElementById('descripcion-error').textContent = '';
+            this.setCustomValidity('');
+        }
+    });
 </script>
+
 

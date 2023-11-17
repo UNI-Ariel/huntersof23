8355db6b8c35257ad773d1732e057368d210f0f4
Jhon Javin Colque Leon <202001796@est.umss.edu>
2023-11-17 16:57:40 -0400

    sección modificar rol

---
+++ auth/updateUser.php
@@ -0,0 +1,63 @@
+<?php
+include('./auth.php');
+
+if (!$authenticated) {
+    header("Location: ./login.php");
+} else {
+    if (!$admin) {
+        header("Location: ./unauth.php");
+    }
+}
+
+include("../utils/dbConnection.php");
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+$users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit User</title>
+    
+</head>
+
+<body>
+    <div class="container">
+        <div class="link">
+            <a class="ca2" href="adminDashboard.php"><strong>Atrás</strong></a>
+            <a style="margin-top:5px;"></a>
+        </div>
+
+        <h2>Usuarios Registrados</h2>
+        <table align="center" border="2" class="displayUser">
+            <tr>
+                <th>No</th>
+                <th>Nombre Usuario</th>
+                <th>Admin</th>
+            </tr>
+            <?php foreach ($users as $index => $user) : ?>
+                <tr>
+                    <td><?php echo $index + 1; ?></td>
+                    <td><?php echo $user['username']; ?></td>
+                    <div class="checkbox-style"></div>
+                    <td><input type="checkbox" name="<?php echo $user['id'] ?>" id="<?php echo $user['id'] ?>" <?php if ($user['groupID'] == 1) : ?> checked <?php endif; ?> <?php if ($user['id'] == $uid) : ?> fromUser <?php endif; ?>></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+        <div class="message">
+        </div>
+    </div>
+    <div class="rolModal">
+        <div class="contentRolModal">
+            <p>Actualización realizada con éxito</p>
+            <button class="close">Aceptar</button>
+        </div>
+    </div>
+</body>
+
+</html>

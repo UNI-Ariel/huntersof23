d9b3ca02fc7b7ed65b4851fdcd886276c37f81ec
Maribel Jamira <maribeljamiram@gmail.com>
2023-11-20 17:43:55 -0400

    "MJM" Modifique agregar cancion a Play List

---
+++ pages/mostrarLista.php
@@ -0,0 +1,63 @@
+<?php
+include("../utils/dbConnection.php");  
+session_start();
+
+// Verificar si el usuario está autenticado
+if (!isset($_SESSION['id'])) {
+    echo "Acceso no autorizado. Por favor, inicia sesión.";
+    exit();
+}
+$userId = $_SESSION['id'];
+
+if (isset($_POST['id'])) {
+    $songId= $_POST['id'];
+    
+// Consulta SQL para obtener datos de la tabla de playlists
+$sqlLista = "SELECT p.idPlay, p.imgPlay, p.nomPlay AS nombre, COUNT(l.idPlay) AS cantidad FROM playlist p LEFT JOIN lista l ON p.idPlay = l.idPlay WHERE p.idUser = $userId GROUP BY p.idPlay, p.nomPlay";
+
+$resultCancion = $conn->query($sqlLista);
+
+if ($resultCancion->num_rows > 0) {
+   // $rowCancion = $resultCancion->fetch_assoc();
+    while ($rowCancion = $resultCancion->fetch_assoc()) {
+    // Imprimir la información adicional de la canción
+    echo '<div class="card-header" data-valor="' . htmlspecialchars($rowCancion["idPlay"]) . '" onclick="registrarCancion(this)" >';
+    echo'<div class="card-body">';
+    echo '<img src="' . $rowCancion["imgPlay"] .'">';
+    echo '</div>';
+    echo '<div class="card-body">';
+    echo '<p>'. $rowCancion["nombre"].'</p>';
+    echo '<p>Canciones: '. $rowCancion["cantidad"] .'</p>';
+    echo '</div></div>';
+    }
+} 
+else{
+    echo "No tienes Listas Creadas";
+}
+}
+else {
+    echo "Error al recuperar el id de la cancion";
+}
+?>
+<script>
+   function registrarCancion(elemento){
+            var songId =<?php echo json_encode($songId); ?>;
+            console.log("Valor de la cancion: " + songId);
+            var playId = elemento.dataset.valor;
+            console.log("Valor de data-valor: " + playId);
+            // Hacer una solicitud AJAX para agregar la canción a la lista de reproducción
+            $.ajax({
+                type: 'POST',
+                url: 'pages/agregar_a_lista.php',
+                data: { idSong: songId, idPlay: playId },
+                success: function (data) {
+                    alert(data);
+                    $('#myModal').hide();
+                   
+                },
+                error: function () {
+                    alert('Error al agregar la canción a la lista de reproducción.');
+                }
+            });
+        }
+</script>
--- pages/searchContent.php
+++ pages/searchContent.php
@@ -37,44 +37,117 @@ if (isset($_GET['search'])) {
                 </div>
                 <div class="func" >
                     <?php echo $heartIcon; ?>
-                    <button class="open-modal-button" onclick="openModal()"><i class="fas fa-plus" ></i></button>
+                    <button class="open-modal-button" data-songid="<?php echo $song['id']; ?>" onclick="openModal(this)" ><i class="fas fa-plus" ></i></button>
                 </div>
             </div>
         <?php endforeach; ?>
     </div>
 </section>
 
+<!------Agregar auna lista de Reproduccion--
+<link rel="stylesheet" href="pages/play.css">-------->
 
-<!------Agregar auna lista de Reproduccion---------->
-<link rel="stylesheet" href="pages/play.css">
+
   <!-- Modal1 -->
   <div id="myModal" class="modal">
     <div class="modal-content">
     <span class="close" onclick="closeModal()">&times;</span>
     <button class="open-modal-button" onclick="openModal2()">Agregar nuevo</button>
       <div id="modalContent">
-       <!-- <img id="imagenModalSrc" src="" alt="Imagen de la base de datos">
-        <p id="nombreImagen"></p>
-        <p id="cantidad"></p>-->
+      
       </div>
     </div>
   </div>
 
-  <!-- Modal2 -->
-  <div id="myModal2" class="modal">
+ <!-- Modal2 -->
+ <div id="myModal2" class="modal">
     <div class="modal-content">
     <span class="close" onclick="closeModal2()">&times;</span>
       <div id="modalContent2">
+      <form id="formularioP">
+        <label for="nombre">Ponle nombre a tu Lista de Reproduccion</label> 
+        <input type="text" id="nombreP" name="nombreP"minlength="2" maxlength="30" pattern="[a-zA-Z0-9]+" title="Solo se permiten caracteres alfanuméricos" required>
+        <span style="color: red;" class="error" id="error"></span>
+        <!-- Agrega más campos según tus necesidades -->
+        <button style="background-color: #979a9e; color: white;  border: none; border-radius: 5px; cursor: pointer; width: 25%; " onclick="closeModal2()">Cancelar</button>
+        <button style="background-color: #266882; color: white;  border: none; border-radius: 5px; cursor: pointer; width: 25%; " type="button" onclick="insertarDatos()">Aceptar</button>
+      </form>
       </div>
     </div>
   </div>
-  <script src="pages/mosPlayList.js"></script>
-  <script src="pages/addPlay.js"></script>
-
-<!------------------------->
 
 
+  <!-- JavaScript (index.php) -->
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
+<script>
+  var datoAlmacenado;
+  function openModal(button) {
+    var modal = document.getElementById('myModal');
+    var dataId = button.getAttribute('data-songid');
 
+// Puedes hacer lo que quieras con el valor data-id, por ejemplo, imprimirlo
+    console.log("ID del botón: " + dataId);
+    datoAlmacenado = dataId;
+            // Hacer una solicitud AJAX para obtener información adicional de la canción
+            $.ajax({
+                type: 'POST',
+                url: 'pages/mostrarLista.php',
+                data: { id: dataId },
+                success: function (data) {
+                  //console.log(data);
+                    // Mostrar los resultados en la ventana modal
+                    $('#modalContent').html(data);
+                   // $('#myModal').modal('show');
+                },
+                error: function () {
+                    alert('Error al obtener la información de la canción.');
+                }
+            });
+   modal.style.display = 'block';
 
+ }
+ function openModal2() {
+    //var modal = document.getElementById('myModal2');
+    $('#myModal').hide();
+    // Muestra el dato en el segundo modal
+    //$('#dato').text(datoAlmacenado);
+    $('#myModal2').show();
+    //alert("Dato usado en otra función: " + datoAlmacenado);
 
+ }
+ function insertarDatos() {
+       // Obtener el valor del campo nombre
+       var nombre = document.getElementById('nombreP').value;
+        var errorNombre = document.getElementById('error');
+
+        // Validar la longitud del campo nombre
+        var expresionRegular = /^[a-zA-Z0-9]+$/;
+        if (nombre.length < 2 || nombre.length > 30 || !expresionRegular.test(nombre)) {
+            errorNombre.textContent = 'El nombre debe tener entre 2 y 30 caracteres alfanumericos.';
+        } else {
+            // Si la validación pasa, puedes enviar los datos al servidor aquí
+            errorNombre.textContent = '';
+            $.ajax({
+            type: 'POST',
+            url: 'pages/addPlay.php',
+            data: { nombre: nombre, idSong: datoAlmacenado},
+            dataType: 'json',
+            success: function(response) {
+                // Puedes manejar la respuesta del servidor aquí
+                console.log(response);
+                alert("Registro exitoso!! ");
+                // Cerrar la ventana modal después de insertar datos
+                $('#myModal2').hide();
+            },
+            error: function(error) {
+                console.error('Error:', error);
+            }
+           });
+            //cerrarModal();
+        }
+    }
+</script>
+
+<!--<script src="pages/mosPlayList.js"></script>-->
+<!-- <script src="pages/addPlay.js"></script>-->

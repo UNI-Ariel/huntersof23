2a383af49718c561d1398441d68f2d2bceadbc15
Maribel Jamira <maribeljamiram@gmail.com>
2023-11-20 18:36:53 -0400

    "MJM" funcion cerrar implementada

--- pages/searchContent.php
+++ pages/searchContent.php
@@ -150,6 +150,10 @@ if (isset($_GET['search'])) {
     function closeModal() {
     var modal = document.getElementById('myModal');
     modal.style.display = 'none';
+    }
+    function closeModal2() {
+    var modal = document.getElementById('myModal2');
+    modal.style.display = 'none';
   }
 </script>
 
